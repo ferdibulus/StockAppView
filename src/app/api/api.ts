7cfd8aed022e375a5f2113cbce303d16c6037787
Ferdi Bulus <ferdi_bulus@hotmail.com>
2022-10-05 21:18:32 +0300

    api updated

--- src/app/api/api.ts
+++ src/app/api/api.ts
@@ -10,12 +10,12 @@ export class Api {
   constructor(private http: HttpClient) {}
 
   saveUser(user:any): Observable<any> {
-    const url = 'http://localhost:8181/users/login';
+    const url = 'http://92.205.30.150:8181/users/login';
     return this.http.post<any>(url, user);
   }
 
   getAllProducts(): Observable<any> {
-    const url = 'http://localhost:8181/products';
+    const url = 'http://92.205.30.150:8181/products';
     return this.http.get<any>(url);
   }
 }
